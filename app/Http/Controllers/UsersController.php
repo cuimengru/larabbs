02d947074f0ba70e9b705e5957a2b02f257264f8
cuimengru <442402330@qq.com>
2020-06-04 02:38:13 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
     //个人信息展示页
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    //资料编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //资料编辑页面 update 方法
+    public function update(UserRequest $request,User $user){
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 
 }
