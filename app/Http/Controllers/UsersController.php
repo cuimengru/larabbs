dde339e538d7ca513431305209d81f18cabb152c
cuimengru <442402330@qq.com>
2020-06-04 03:20:34 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -20,9 +21,18 @@ class UsersController extends Controller
     }
 
     //资料编辑页面 update 方法
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
 }
