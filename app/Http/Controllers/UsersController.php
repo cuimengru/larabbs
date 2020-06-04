a81a8b9945b1ac2d944995893fbf51c767f5ffd5
cuimengru <442402330@qq.com>
2020-06-04 05:59:40 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //个人信息展示页
     public function show(User $user){
         return view('users.show',compact('user'));
@@ -17,11 +22,13 @@ class UsersController extends Controller
     //资料编辑页面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //资料编辑页面 update 方法
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
