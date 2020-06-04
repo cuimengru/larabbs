e73a05d7b2c9eb3d162476ad665b139523859ced
cuimengru <442402330@qq.com>
2020-06-04 08:30:30 +0000

    填充用户和话题数据

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //个人信息页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+//话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
