4b85aa6c7265dc3dedfc4ce88f605a7e5385c223
cuimengru <442402330@qq.com>
2020-06-05 02:55:29 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,8 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 //话题
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 //分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
