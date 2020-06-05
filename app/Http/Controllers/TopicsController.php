80dc66b60f5a84049721d1b831aeb8b701a1faca
cuimengru <442402330@qq.com>
2020-06-05 10:47:14 +0000

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
+use App\Models\Link;
 use Auth;
 
 class TopicsController extends Controller
@@ -18,14 +19,15 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic,User $user)
+	public function index(Request $request, Topic $topic,User $user,Link $link)
 	{
         $topics = $topic->withOrder($request->order)
             ->with('user', 'category')  // 预加载防止 N+1 问题
             ->paginate(20);
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
 //        dd($active_users);
-		return view('topics.index', compact('topics','active_users'));
+		return view('topics.index', compact('topics','active_users','links'));
 	}
 
     public function show(Request $request, Topic $topic)
