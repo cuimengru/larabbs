84d70f7795c7df35c47e69cf8320d3317a130873
cuimengru <442402330@qq.com>
2020-06-06 03:15:52 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,9 @@ use Illuminate\Http\Request;
 
 //测试路由
 Route::prefix('v1')->name('api.v1.')->group(function() {
-    Route::get('version', function() {
-        abort(403, 'test');
-        return 'this is version v1';
-    })->name('version');
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
