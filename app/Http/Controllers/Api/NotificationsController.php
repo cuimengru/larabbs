43c989c674ecc5bbf290221b37249b033442dd7e
cuimengru <442402330@qq.com>
2020-06-08 07:40:07 +0000

    消息已读

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -22,4 +22,12 @@ class NotificationsController extends Controller
             'unread_count' => $request->user()->notification_count,
         ]);
     }
+
+    //已读消息
+    public function read(Request $request)
+    {
+        $request->user()->markAsRead();
+
+        return response(null, 204);
+    }
 }
