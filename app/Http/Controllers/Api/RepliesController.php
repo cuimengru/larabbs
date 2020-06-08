fb52338ff006abfd786e4bc69a3c9633ede3b8b4
cuimengru <442402330@qq.com>
2020-06-08 07:20:57 +0000

    回复列表

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Topic;
 use App\Models\Reply;
 use Illuminate\Http\Request;
+use App\Http\Queries\ReplyQuery;
 use App\Http\Resources\ReplyResource;
 use App\Http\Requests\Api\ReplyRequest;
 
@@ -30,5 +31,21 @@ class RepliesController extends Controller
         $reply->delete();
 
         return response(null, 204);
+    }
+
+    //某个话题的回复列表
+    public function index($topicId, ReplyQuery $query)
+    {
+        $replies = $query->where('topic_id', $topicId)->paginate();
+
+        return ReplyResource::collection($replies);
+    }
+
+    //某个用户的回复列表
+    public function userIndex($userId, ReplyQuery $query)
+    {
+        $replies = $query->where('user_id', $userId)->paginate();
+
+        return ReplyResource::collection($replies);
     }
 }
